06dfeb84ed0767fc4826448c55fb4406aeee0dc6
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-07-30 21:40:32 +0100

    fetch products from database
    title, display is a WIP but the data is being successfully brought to the frontend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { setupProductsDB } from './db/db.js';
 import productRouter from './routes/productRoutes.js';
+import cors from 'cors'
 
 const app = express();
 const port = 3000;
 
 app.use(express.json())
+app.use(cors())
 
 
 

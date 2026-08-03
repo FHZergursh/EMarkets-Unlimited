0dda51c22e4b6e35ac69445e4627d04a4408061d
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-08-03 20:40:35 +0100

    add users and reviews tables, also create a placeholder Product page
    title, Product page will display information of products that get clicked on

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -17,6 +17,29 @@ export async function setupProductsDB() {
         stock INT 
       );
     `;
+
+    await sql`
+      CREATE TABLE IF NOT EXISTS users (
+        userid SERIAL PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(500) NOT NULL,
+
+      );
+    `;
+
+    await sql`
+      CREATE TABLE IF NOT EXISTS reviews (
+        reviewid SERIAL PRIMARY KEY,
+        FOREIGN KEY (productid) REFERENCES products(productid)
+        content VARCHAR(1000) NOT NULL,
+        starrating INT, 
+        createdat TIMESTAMP NOT NULL,
+
+      );
+    `;
+
+
     console.log('Finished initialising table.');
 
   } catch (err) {
---
+++ frontend/src/pages/Product.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+      <div>Product name, from props props</div>
+
+      <img> Image placeholder </img>
+
+      <div>
+        <div>Price info </div>
+        <div>Seller name and account</div>
+      </div>
+
+      <div>Reviews, secondary key db probably?</div>
+
+      <div></div>
+
+
+
+    </div>
+  )
+}
+
+export default Product
